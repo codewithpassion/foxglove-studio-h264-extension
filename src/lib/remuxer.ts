c3c24b3b7f22aaffa47841364b4ebbbb1bb3a08a
Dominik Fretz <dominik.fretz@gmail.com>
2022-10-24 14:09:23 +1100

    d

--- src/lib/remuxer.ts
+++ src/lib/remuxer.ts
@@ -120,7 +120,7 @@ export class Remuxer {
         height: this.sps.picHeight,
         // timescale: this.sps.time_scale ?? 1000,
         timescale: 1000,
-        duration: 0,
+        duration: 1000,
         samples: [],
       };
       return track;
